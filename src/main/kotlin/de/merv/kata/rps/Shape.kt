24af5d67dfcc1338e9bf2e29b919591fa3011d36
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-01 15:31:30 -0400

    Introduce Rock Paper Scissors winner evaluation

--- src/main/kotlin/de/merv/kata/rps/Shape.kt
+++ src/main/kotlin/de/merv/kata/rps/Shape.kt
@@ -1,7 +1,29 @@
 package de.merv.kata.rps
 
-enum class Shape {
+enum class Shape : ShapeInterface {
     ROCK,
     PAPER,
-    SCISSORS
+    SCISSORS,
+    ;
+
+    companion object {
+        private val winnersMap = hashMapOf(
+                ROCK to SCISSORS,
+                PAPER to ROCK,
+                SCISSORS to PAPER
+        )
+    }
+
+    override fun winAgainst(opponent: Shape): Boolean {
+        val possibleLoser = winnersMap[this]
+        if (possibleLoser != null) {
+            return possibleLoser == opponent
+        }
+
+        throw IllegalStateException("Shape is unknown to the rules")
+    }
+}
+
+interface ShapeInterface {
+    fun winAgainst(opponent: Shape): Boolean
 }
--- src/test/kotlin/de/merv/kata/rps/ShapeSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/ShapeSpec.kt
@@ -1,8 +1,11 @@
 package de.merv.kata.rps
 
+import io.kotlintest.data.forall
 import io.kotlintest.matchers.collections.containExactlyInAnyOrder
 import io.kotlintest.should
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.BehaviorSpec
+import io.kotlintest.tables.row
 
 class ShapeSpec : BehaviorSpec({
 
@@ -10,6 +13,24 @@ class ShapeSpec : BehaviorSpec({
         `when`("I gather all existing shapes") {
             then("Exactly Rock, Paper and Scissors should exist") {
                 enumValues<Shape>().toList() should containExactlyInAnyOrder(Shape.ROCK, Shape.PAPER, Shape.SCISSORS)
+            }
+        }
+
+        `when`("They play against each other") {
+            then("They should win according to the rules") {
+                forall(
+                        row(Shape.ROCK, Shape.ROCK, false),
+                        row(Shape.ROCK, Shape.PAPER, false),
+                        row(Shape.ROCK, Shape.SCISSORS, true),
+                        row(Shape.PAPER, Shape.ROCK, true),
+                        row(Shape.PAPER, Shape.PAPER, false),
+                        row(Shape.PAPER, Shape.SCISSORS, false),
+                        row(Shape.SCISSORS, Shape.ROCK, false),
+                        row(Shape.SCISSORS, Shape.PAPER, true),
+                        row(Shape.SCISSORS, Shape.SCISSORS, false)
+                ) { shapeOne, shapeTwo, expected ->
+                    shapeOne.winAgainst(shapeTwo) shouldBe expected
+                }
             }
         }
     }
