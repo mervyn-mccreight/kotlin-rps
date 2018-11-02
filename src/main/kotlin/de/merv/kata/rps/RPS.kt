7bd2b077147082d28cb48c1328e1ce2050070a16
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-02 12:45:44 -0400

    Introduce main method and tell gradle to create a fat jar

--- src/main/kotlin/de/merv/kata/rps/RPS.kt
+++ src/main/kotlin/de/merv/kata/rps/RPS.kt
@@ -1,5 +1,16 @@
 package de.merv.kata.rps
 
-fun main(args : Array<String>) {
-    println("Hello, world!")
+object RPS {
+
+    @JvmStatic
+    fun main(args : Array<String>) {
+        val rockPlayer = {Player.playOneOf(Shape.ROCK)}
+        val randomPlayer = {Player.playOneOf(*enumValues())}
+        val (win, draw, loss) = Simulation.playGames(rockPlayer, randomPlayer)
+
+        println("Rock player wins = $win")
+        println("Random player wins = $loss")
+        println("Draws = $draw")
+    }
 }
+
