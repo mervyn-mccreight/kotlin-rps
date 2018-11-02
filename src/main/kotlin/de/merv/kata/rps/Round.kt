d7b203139ef606707cd8266df62d01d2d48f9caf
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-02 10:19:43 -0400

    Implement winning and losing logic into Round

--- src/main/kotlin/de/merv/kata/rps/Round.kt
+++ src/main/kotlin/de/merv/kata/rps/Round.kt
@@ -2,11 +2,15 @@ package de.merv.kata.rps
 
 object Round {
 
+    private val DRAW = Triple(0, 1, 0)
+    private val WIN = Triple(1, 0, 0)
+    private val LOSE = Triple(0, 0, 1)
+
     fun play(contender: Shape, opponent: Shape): Triple<Int, Int, Int> {
         if (contender == opponent) {
-            return Triple(0, 1, 0)
+            return DRAW
         }
 
-        TODO("Implement")
+        return if (contender winsAgainst opponent) WIN else LOSE
     }
 }
--- src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
@@ -1,8 +1,10 @@
 package de.merv.kata.rps
 
+import io.kotlintest.data.forall
 import io.kotlintest.properties.assertAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
 
 class RoundSpec : StringSpec() {
     init {
@@ -11,6 +13,19 @@ class RoundSpec : StringSpec() {
 
             assertAll(ShapeGenerator()) { shape ->
                 Round.play(shape, shape) shouldBe Triple(0, 1, 0)
+            }
+        }
+
+        "A round with a winner should evaluate according to the rules" {
+            forall(
+                    row(Shape.ROCK, Shape.PAPER, Triple(0, 0, 1)),
+                    row(Shape.ROCK, Shape.SCISSORS, Triple(1, 0, 0)),
+                    row(Shape.PAPER, Shape.ROCK, Triple(1, 0, 0)),
+                    row(Shape.PAPER, Shape.SCISSORS, Triple(0, 0, 1)),
+                    row(Shape.SCISSORS, Shape.ROCK, Triple(0, 0, 1)),
+                    row(Shape.SCISSORS, Shape.PAPER, Triple(1, 0, 0))
+            ) { shapeOne, shapeTwo, expected ->
+                Round.play(shapeOne, shapeTwo) shouldBe expected
             }
         }
     }
