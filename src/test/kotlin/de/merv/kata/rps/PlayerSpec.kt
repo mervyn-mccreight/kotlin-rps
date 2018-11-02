c5d56718cce8b9c18172d4b729e0db6f72a491d9
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-02 10:05:51 -0400

    Rewrite PlayerSpec to use matchers for more expressiveness

--- src/test/kotlin/de/merv/kata/rps/PlayerSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/PlayerSpec.kt
@@ -1,21 +1,23 @@
 package de.merv.kata.rps
 
-import io.kotlintest.properties.forAll
+import io.kotlintest.be
+import io.kotlintest.properties.assertAll
+import io.kotlintest.should
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class PlayerSpec : StringSpec() {
 
     init {
         "A player with only one choice should always play exactly this choice" {
-            forAll(ShapeGenerator()) { singleShape: Shape ->
-                Player.playOneOf(singleShape) == singleShape
+            assertAll(ShapeGenerator()) { singleShape: Shape ->
+                Player.playOneOf(singleShape) shouldBe singleShape
             }
         }
 
         "A player with more choices should always play one of the given choices" {
-            forAll(ShapeGenerator(), ShapeGenerator()) { shapeOne: Shape, shapeTwo: Shape ->
-                val chosenShape = Player.playOneOf(shapeOne, shapeTwo)
-                chosenShape == shapeOne || chosenShape == shapeTwo
+            assertAll(ShapeGenerator(), ShapeGenerator()) { shapeOne: Shape, shapeTwo: Shape ->
+                Player.playOneOf(shapeOne, shapeTwo) should be(shapeOne).or(be(shapeTwo))
             }
         }
     }
