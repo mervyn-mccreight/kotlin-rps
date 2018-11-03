2cf9c5f3a247219b2fb54f893db2e320ff91f479
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-03 10:22:03 -0400

    Correct formatting

--- src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
@@ -7,10 +7,9 @@ import io.kotlintest.specs.StringSpec
 import io.kotlintest.tables.row
 
 class RoundSpec : StringSpec() {
+
     init {
-
         "A round with two equal shapes should always be evaluated as a draw" {
-
             assertAll(ShapeGenerator()) { shape ->
                 Round.play(shape, shape) shouldBe Triple(0, 1, 0)
             }
