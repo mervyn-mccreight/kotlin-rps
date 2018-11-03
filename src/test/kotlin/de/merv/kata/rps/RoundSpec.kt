c457bb19fe9f3489da3a43ddada2103c3284ea2f
Mervyn McCreight <mervyn.mccreight@gmail.com>
2018-11-03 10:23:04 -0400

    Use static imports to improve readability

--- src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
@@ -1,5 +1,6 @@
 package de.merv.kata.rps
 
+import de.merv.kata.rps.Shape.*
 import io.kotlintest.data.forall
 import io.kotlintest.properties.assertAll
 import io.kotlintest.shouldBe
@@ -17,12 +18,12 @@ class RoundSpec : StringSpec() {
 
         "A round with a winner should evaluate according to the rules" {
             forall(
-                    row(Shape.ROCK, Shape.PAPER, Triple(0, 0, 1)),
-                    row(Shape.ROCK, Shape.SCISSORS, Triple(1, 0, 0)),
-                    row(Shape.PAPER, Shape.ROCK, Triple(1, 0, 0)),
-                    row(Shape.PAPER, Shape.SCISSORS, Triple(0, 0, 1)),
-                    row(Shape.SCISSORS, Shape.ROCK, Triple(0, 0, 1)),
-                    row(Shape.SCISSORS, Shape.PAPER, Triple(1, 0, 0))
+                    row(ROCK, PAPER, Triple(0, 0, 1)),
+                    row(ROCK, SCISSORS, Triple(1, 0, 0)),
+                    row(PAPER, ROCK, Triple(1, 0, 0)),
+                    row(PAPER, SCISSORS, Triple(0, 0, 1)),
+                    row(SCISSORS, ROCK, Triple(0, 0, 1)),
+                    row(SCISSORS, PAPER, Triple(1, 0, 0))
             ) { shapeOne, shapeTwo, expected ->
                 Round.play(shapeOne, shapeTwo) shouldBe expected
             }
