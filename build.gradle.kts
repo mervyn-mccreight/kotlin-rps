865958746899454b49048d242dd49e9b848727d8
Mervyn McCreight <mervyn-mccreight@users.noreply.github.com>
2022-08-20 11:03:26 +0200

    Use kotlin syntax to configure the gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
+}
+
+group = "de.merv.kata"
+version = "1.0"
+
+application {
+    mainClass.set("de.merv.kata.rps.RPS")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest:kotest-property:5.4.2")
+    testImplementation("io.kotest:kotest-framework-datatest:5.4.2")
+}
