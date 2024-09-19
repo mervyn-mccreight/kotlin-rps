16d04681f636007f434279d84ed2f0b198de7425
Mervyn McCreight <mervyn-mccreight@users.noreply.github.com>
2024-09-20 00:40:35 +0200

    Don't rely on unstable `Triple::toString` usage

--- src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/RoundSpec.kt
@@ -6,41 +6,44 @@ import de.merv.kata.rps.Shape.SCISSORS
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
-import io.kotest.property.Exhaustive
-import io.kotest.property.checkAll
-import io.kotest.property.exhaustive.enum
 
 class RoundSpec : FunSpec() {
-
-    companion object {
-        private val WIN = Triple(1, 0, 0)
-        private val DRAW = Triple(0, 1, 0)
-        private val LOSE = Triple(0, 0, 1)
-    }
-
     init {
-        test("A round with two equal shapes should always be evaluated as a draw") {
-            checkAll(Exhaustive.enum()) { shape: Shape ->
-                Round.play(shape, shape) shouldBe DRAW
+        context("A round with two equal shapes should always be evaluated as a draw") {
+            withData(Shape.entries.map { Draw(it, it) }) {
+                Round.play(it.challenger, it.opponent) shouldBe it.expectedResult()
             }
         }
 
         context("A round with a winner should evaluate according to the rules") {
-            data class RulesTestCase(
-                val challenger: Shape,
-                val opponent: Shape,
-                val expectedResult: Triple<Int, Int, Int>
-            )
             withData(
-                RulesTestCase(ROCK, PAPER, LOSE),
-                RulesTestCase(ROCK, SCISSORS, WIN),
-                RulesTestCase(PAPER, ROCK, WIN),
-                RulesTestCase(PAPER, SCISSORS, LOSE),
-                RulesTestCase(SCISSORS, ROCK, LOSE),
-                RulesTestCase(SCISSORS, PAPER, WIN)
-            ) { (challenger, opponent, expectedResult) ->
-                Round.play(challenger, opponent) shouldBe expectedResult
+                Loss(ROCK, PAPER),
+                Win(ROCK, SCISSORS),
+                Win(PAPER, ROCK),
+                Loss(PAPER, SCISSORS),
+                Loss(SCISSORS, ROCK),
+                Win(SCISSORS, PAPER)
+            ) {
+                Round.play(it.challenger, it.opponent) shouldBe it.expectedResult()
             }
         }
     }
+}
+
+private sealed interface RulesTestCase {
+    val challenger: Shape
+    val opponent: Shape
+    fun expectedResult(): Triple<Int, Int, Int>
+}
+
+private data class Win(override val challenger: Shape, override val opponent: Shape) : RulesTestCase {
+    override fun expectedResult(): Triple<Int, Int, Int> = Triple(1, 0, 0)
+}
+
+private data class Loss(override val challenger: Shape, override val opponent: Shape) : RulesTestCase {
+    override fun expectedResult(): Triple<Int, Int, Int> = Triple(0, 0, 1)
+}
+
+private data class Draw(override val challenger: Shape, override val opponent: Shape) : RulesTestCase {
+    override fun expectedResult(): Triple<Int, Int, Int> = Triple(0, 1, 0)
 }
