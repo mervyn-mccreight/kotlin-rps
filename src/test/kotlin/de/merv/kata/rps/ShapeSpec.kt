77af7d20b4889d1b689947385deec6d63265575c
mervyn-mccreight <mervyn-mccreight@users.noreply.github.com>
2025-09-18 03:37:30 +0200

    test: replace kotest with test-balloon
    Closes #146

--- src/test/kotlin/de/merv/kata/rps/ShapeSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/ShapeSpec.kt
@@ -1,49 +1,46 @@
 package de.merv.kata.rps
 
+import de.infix.testBalloon.framework.testSuite
 import de.merv.kata.rps.Shape.PAPER
 import de.merv.kata.rps.Shape.ROCK
 import de.merv.kata.rps.Shape.SCISSORS
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.datatest.withData
-import io.kotest.matchers.be
-import io.kotest.matchers.or
-import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
-import io.kotest.property.Arb
-import io.kotest.property.arbitrary.enum
-import io.kotest.property.checkAll
 
-class ShapeSpec : FunSpec({
-    context("Shapes") {
-        context("should win against each others according to the rules") {
-            data class WinsAgainstTestCase(val challenger: Shape, val opponent: Shape, val expected: Boolean)
-            withData(
-                WinsAgainstTestCase(ROCK, ROCK, false),
-                WinsAgainstTestCase(ROCK, PAPER, false),
-                WinsAgainstTestCase(ROCK, SCISSORS, true),
-                WinsAgainstTestCase(PAPER, ROCK, true),
-                WinsAgainstTestCase(PAPER, PAPER, false),
-                WinsAgainstTestCase(PAPER, SCISSORS, false),
-                WinsAgainstTestCase(SCISSORS, ROCK, false),
-                WinsAgainstTestCase(SCISSORS, PAPER, true),
-                WinsAgainstTestCase(SCISSORS, SCISSORS, false)
-            ) { (challenger, opponent, expected) ->
-                challenger winsAgainst opponent shouldBe expected
-            }
-        }
+val ShapeSpec by testSuite {
+    data class WinsAgainstTestCase(val challenger: Shape, val opponent: Shape, val expected: Boolean)
 
-        context("One picks a random shape") {
-            test("He always picks the same shape if the set of choices only contains one shape") {
-                checkAll(Arb.enum()) { singleShape: Shape ->
-                    Shape.pickOneOf(singleShape) shouldBe singleShape
-                }
-            }
-
-            test("He always picks a shape out of the set of choices and nothing else") {
-                checkAll(Arb.enum(), Arb.enum()) { shapeOne: Shape, shapeTwo: Shape ->
-                    Shape.pickOneOf(shapeOne, shapeTwo) should be(shapeOne).or(be(shapeTwo))
-                }
+    testSuite("should win against each other according to the rules") {
+        listOf(
+            WinsAgainstTestCase(ROCK, ROCK, false),
+            WinsAgainstTestCase(ROCK, PAPER, false),
+            WinsAgainstTestCase(ROCK, SCISSORS, true),
+            WinsAgainstTestCase(PAPER, ROCK, true),
+            WinsAgainstTestCase(PAPER, PAPER, false),
+            WinsAgainstTestCase(PAPER, SCISSORS, false),
+            WinsAgainstTestCase(SCISSORS, ROCK, false),
+            WinsAgainstTestCase(SCISSORS, PAPER, true),
+            WinsAgainstTestCase(SCISSORS, SCISSORS, false)
+        ).forEach {
+            test(it.toString()) {
+                assert((it.challenger winsAgainst it.opponent) == it.expected)
             }
         }
     }
-})
+
+    testSuite("One picks a random shape") {
+        test("He always picks the same shape if the set of choices only contains one shape") {
+            Shape.entries.forEach {
+                assert(Shape.pickOneOf(it) == it)
+            }
+        }
+
+        test("He always picks a shape out of the set of choices and nothing else") {
+            repeat(100) {
+                val shapeOne = Shape.entries.random()
+                val shapeTwo = Shape.entries.random()
+
+                val pick = Shape.pickOneOf(shapeOne, shapeTwo)
+                assert(pick == shapeOne || pick == shapeTwo)
+            }
+        }
+    }
+}
--- src/test/kotlin/de/merv/kata/rps/SimulationSpec.kt
+++ src/test/kotlin/de/merv/kata/rps/SimulationSpec.kt
@@ -1,28 +1,24 @@
 package de.merv.kata.rps
 
+import de.infix.testBalloon.framework.testSuite
 import de.merv.kata.rps.Shape.Companion.pickOneOf
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-import io.kotest.property.Arb
-import io.kotest.property.arbitrary.enum
-import io.kotest.property.checkAll
 
-class SimulationSpec : StringSpec() {
-    init {
-        "A simulation of two players always picking the same shape should result in 100-draws" {
-            checkAll(Arb.enum()) { shape: Shape ->
+val SimulationSpec by testSuite {
+    test("A simulation of two players always picking the same shape should result in 100-draws") {
+        Shape
+            .entries
+            .forEach { shape ->
                 val playerOne: Player = { pickOneOf(shape) }
                 val playerTwo: Player = { pickOneOf(shape) }
 
-                Simulation.playGames(playerOne, playerTwo) shouldBe Triple(0, 100, 0)
+                assert(Simulation.playGames(playerOne, playerTwo) == Triple(0, 100, 0))
             }
-        }
+    }
 
-        "A simulation should always result in 100 played rounds" {
-            val randomPlayer = { pickOneOf(*Shape.entries.toTypedArray()) }
-            val (win, draw, loss) = Simulation.playGames(randomPlayer, randomPlayer)
+    test("A simulation should always result in 100 played rounds") {
+        val randomPlayer = { pickOneOf(*Shape.entries.toTypedArray()) }
+        val (win, draw, loss) = Simulation.playGames(randomPlayer, randomPlayer)
 
-            win + draw + loss shouldBe 100
-        }
+        assert((win + draw + loss) == 100)
     }
 }
